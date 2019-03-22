e8161f3342d2b465b8cec38afc2eb201fdfe0570
DonMahallem <donmahallem@users.noreply.github.com>
2019-03-22 15:45:07 +0100

    added vehicle locations types

---
+++ src/vehicle-locations.ts
@@ -0,0 +1,38 @@
+export interface IBaseVehicleLocation {
+    id: string;
+}
+
+export interface IPathSegment {
+    y1: number;
+    x1: number;
+    y2: number;
+    angle: number;
+    x2: number;
+    length: number;
+}
+export interface IDeletedVehicleLocation extends IBaseVehicleLocation {
+    isDeleted: true;
+}
+export interface IVehicleLocation extends IBaseVehicleLocation {
+    category: string;
+    color: string;
+    heading: number;
+    latitude: number;
+    longitude: number;
+    name: string;
+    path: IPathSegment[];
+    tripId: string;
+}
+
+export type VehicleLocations = IDeletedVehicleLocation | IVehicleLocation;
+
+export interface IVehicleLocationList {
+    /**
+     * Timestamp
+     */
+    lastUpdate: number;
+    /**
+     * reported locations
+     */
+    vehicles: VehicleLocations[];
+}
