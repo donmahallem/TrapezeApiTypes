6838e301559800f0ac2351c4eea87d379db9a611
DonMahallem <donmahallem@users.noreply.github.com>
2019-04-06 14:19:03 +0200

    fix naming

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from "./vehicle-locations";
 export * from "./vehicle-path-info";
 export * from "./trip-passages";
 export * from "./stop-info";
+export * from "./stop-passage";
 export * from "./settings";
 export * from "./trip-route";
