8f2449f2d0c82a32a35fc536860b08c9dda39472
DonMahallem <donmahallem@users.noreply.github.com>
2019-04-06 11:11:05 +0200

    settings type added

--- src/settings.ts
+++ src/settings.ts
@@ -1,8 +1,19 @@
+type CountryCodes = ["ab", "aa", "af", "sq", "am", "ar", "hy", "as", "ay", "az",
+    "ba", "eu", "bn", "dz", "bh", "bi", "br", "bg", "my", "be", "km", "ca", "zh", "co", "hr", "cs",
+    "da", "nl", "en", "eo", "et", "fo", "fj", "fi", "fr", "fy", "gd", "gl", "ka", "de", "el", "kl",
+    "gn", "gu", "ha", "iw", "hi", "hu", "is", "in", "ia", "ie", "ik", "ga", "it", "ja", "jw", "kn",
+    "ks", "kk", "rw", "ky", "rn", "ko", "ku", "lo", "la", "lv", "ln", "lt", "mk", "mg", "ms", "ml",
+    "mt", "mi", "mr", "mo", "mn", "na", "ne", "no", "oc", "or", "om", "ps", "fa", "pl", "pt", "pa",
+    "qu", "rm", "ro", "ru", "sm", "sg", "sa", "sr", "sh", "st", "tn", "sn", "sd", "si", "ss", "sk",
+    "sl", "so", "es", "su", "sw", "sv", "tl", "tg", "ta", "tt", "te", "th", "bo", "ti", "to", "ts",
+    "tr", "tk", "tw", "uk", "ur", "uz", "vi", "vo", "cy", "wo", "xh", "ji", "yo", "zu"];
+type ValuesOf<T extends CountryCodes> = T[number];
+type AnyCombination<T extends CountryCodes> = Array<T[keyof T]>;
 export interface ISettings {
     /**
      * eg. de en ar
      */
-    AVAILABLE_LANGUAGES: string[];
+    AVAILABLE_LANGUAGES: AnyCombination<CountryCodes>;
     DEFAULT_TIME_PREVIEW: number;
     GEOLOCATION_ENABLED: boolean;
     INITIAL_LAT: number;
@@ -12,7 +23,7 @@ export interface ISettings {
      * language code
      * eg. de en ar
      */
-    LANGUAGE: string;
+    LANGUAGE: ValuesOf<CountryCodes>;
     MAP_ENABLED: boolean;
     MAP_SHOW_CONTROLS: boolean;
     MAP_SHOW_PATTERNS: boolean;
