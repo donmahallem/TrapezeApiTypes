536d18fe202824d0adbc904e6b6dfc5c2cfcc167
donmahallem <donmahallem@users.noreply.github.com>
2019-09-14 21:09:32 +0200

    added few comments

--- src/stop-locations.ts
+++ src/stop-locations.ts
@@ -8,11 +8,29 @@ import { StopId, StopShortName } from "./type-util";
  * @since 1.1.0
  */
 export interface IStopLocation {
+    /**
+     * Type of vehicle
+     */
     category: string | "bus";
+    /**
+     * Stop Id 
+     */
     id: StopId;
+    /**
+     * Latitude in arcmiliseconds
+     */
     latitude: number;
+    /**
+     * Longitutde in arcmiliseconds
+     */
     longitude: number;
+    /**
+     * Humanreadable name of the stop
+     */
     name: string;
+    /**
+     * Stop short id
+     */
     shortName: StopShortName;
 }
 
